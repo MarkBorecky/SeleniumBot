9dea66ed2059e12730307ec69591fffb23351631
MarkBorecki <marek.borecki88@gmail.com>
2024-05-13 21:12:59 +0300

    remove configuration from yaml

--- src/main/java/pl/seleniumbot/configuration/AccountConfiguration.java
+++ src/main/java/pl/seleniumbot/configuration/AccountConfiguration.java
@@ -1,39 +1,10 @@
 package pl.seleniumbot.configuration;
 
 import lombok.Getter;
-import org.yaml.snakeyaml.LoaderOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @Getter
 public class AccountConfiguration {
-    private static final String configurationFileName = "./src/main/resources/account.yaml";
-    private final Account account;
-
-    public AccountConfiguration() {
-        Yaml yaml = new Yaml(new Constructor(Account.class, new LoaderOptions()));
-        try {
-            InputStream inputStream = Files.newInputStream(Paths.get(configurationFileName));
-            this.account = yaml.load(inputStream);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Cannot read configuration file", e);
-        }
-    }
-
-    public String getUrl() {
-        return account.getUrl();
-    }
-
-    public CharSequence getLogin() {
-        return account.getUrl();
-    }
-
-    public CharSequence getPassword() {
-        return account.getPassword();
-    }
+    private final String login = "konfucjusz";
+    private final String password = "SuomenMestari4";
+    private final String url = "https://ts20.x2.europe.travian.com";
 }
